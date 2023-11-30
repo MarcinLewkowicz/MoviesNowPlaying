d82b93b929bb78b6e50275482117693ec78c0a1d
Marcin Lewkowicz <marcin.lewkowicz@gmail.com>
2023-11-30 13:59:22 +0100

    switched to view binding in MainActivity

--- app/src/main/java/pl/ml/demo/movies/ui/MainActivity.kt
+++ app/src/main/java/pl/ml/demo/movies/ui/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import pl.ml.demo.movies.R
+import pl.ml.demo.movies.databinding.ActivityMainBinding
 import pl.ml.demo.movies.domain.model.MovieScreenItem
 import pl.ml.demo.movies.ui.movies.MoviesFragmentDirections
 
@@ -20,10 +21,11 @@ class MainActivity : AppCompatActivity(), MainNavigationInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         navController = navHostFragment.navController
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
